812ff154cbbd9ed6ddc2fd815e5e356badd81681
JC BUISSON <buisson@enseeiht.fr>
2025-04-04 10:06:23 +0200

    WIP

---
+++ frontend/src/use/useTabs.js
@@ -0,0 +1,9 @@
+
+export const tabs = [
+   { uid: 'user_management', name: "Gestion utilisateurs" },
+   { uid: 'group_management', name: "Gestion des groupes" },
+   { uid: 'test_management', name: "Gestion des tests" },
+   { uid: 'student_followup', name: "Suivi des étudiants" },
+   { uid: 'shdl_sandbox', name: "SHDL Sandbox" },
+   { uid: 'craps_sandbox', name: "CRAPS sandbox" },
+]
