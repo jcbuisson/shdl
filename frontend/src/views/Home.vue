11ac463860f09376366603c8d3ce26a58378c55f
JC BUISSON <buisson@enseeiht.fr>
2025-05-21 08:07:18 +0200

    WIP

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,40 +1,61 @@
 <template>
-   <v-toolbar extended>
-      <v-toolbar-title text="SHDL / CRAPS"></v-toolbar-title>
+   <!--
+      - d-flex flex-column fill-height makes the layout a vertical stack filling the full screen.
+      - <v-toolbar> is fixed-height and does not grow.
+      - .router-container uses flex: 1 to take up the remaining space.
+      - overflow: auto ensures scrollbars appear only if needed, inside router-view.
+   -->
+   <v-app class="app-layout">
+      <div class="d-flex flex-column fill-height">
+         <!-- Toolbar (does not grow) -->
+         <v-toolbar extended>
+            <v-toolbar-title text="SHDL / CRAPS"></v-toolbar-title>
 
-      <template v-slot:append>
-         <div class="d-flex ga-2">
-            <v-btn size="small" @click="clearCaches">Clear</v-btn>
-            <OnlineButton :isOnline="isConnected" @connect="connect" @disconnect="disconnect"></OnlineButton>
-            <GithubLink url="https://github.com/jcbuisson/shdl" svgPath="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"></GithubLink>
-         </div>
-         
-         <v-menu :location="location">
-            <template v-slot:activator="{ props }">
-               <v-btn v-bind="props">
-                  {{ isAuthenticated ? signedinUserFullname : "Connexion" }}
-               </v-btn>
+            <template v-slot:append>
+               <div class="d-flex ga-2">
+                  <v-btn size="small" @click="clearCaches">Clear</v-btn>
+                  <OnlineButton :isOnline="isConnected" @connect="connect" @disconnect="disconnect" />
+                  <GithubLink
+                     url="https://github.com/jcbuisson/shdl"
+                     svgPath="M12 2A10 10 0 0 0 2 12..."
+                  />
+               </div>
+
+               <v-menu :location="location">
+                  <template v-slot:activator="{ props }">
+                     <v-btn v-bind="props">
+                        {{ isAuthenticated ? signedinUserFullname : "Connexion" }}
+                     </v-btn>
+                  </template>
+
+                  <v-list>
+                     <v-list-item v-if="isAuthenticated">
+                        <v-list-item-title @click="logout">Déconnexion</v-list-item-title>
+                     </v-list-item>
+                  </v-list>
+               </v-menu>
             </template>
 
-            <v-list>
-               <v-list-item  v-if="isAuthenticated">
-                  <v-list-item-title @click="logout">Déconnexion</v-list-item-title>
-               </v-list-item>
-            </v-list>
-         </v-menu>
-      </template>
+            <template v-slot:extension>
+               <v-tabs slider-color="indigo" v-model="routeTabIndex">
+                  <v-tab
+                     :to="{ path: `/home/${signedinUid}/${tab.uid}` }"
+                     router
+                     v-for="tab in userTabs"
+                     :key="tab.uid"
+                  >
+                     {{ tab.name }}
+                  </v-tab>
+               </v-tabs>
+            </template>
+         </v-toolbar>
 
-      <template v-slot:extension>
-         <v-tabs slider-color="indigo" v-model="routeTabIndex">
-            <v-tab :to="{path: `/home/${signedinUid}/${tab.uid}`}" router v-for="tab in userTabs" :key="tab.uid">
-               {{ tab.name }}
-            </v-tab>
-         </v-tabs>
-      </template>
-   </v-toolbar>
-
-   <router-view></router-view>
-
+         <!-- Fills remaining space -->
+         <div class="router-container">
+            <router-view />
+         </div>
+      </div>
+   </v-app>
 </template>
 
 <script setup>
@@ -129,3 +150,17 @@ async function logout() {
    await restartApp()
 }
 </script>
+
+<style scoped>
+.app-layout {
+  height: 100vh;
+  overflow: hidden;
+}
+
+.router-container {
+  flex: 1 1 auto;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+}
+</style>
