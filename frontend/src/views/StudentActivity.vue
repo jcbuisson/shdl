b838384459cfa0108b38681134206db125b5853e
JC BUISSON <buisson@enseeiht.fr>
2025-05-29 14:59:24 +0200

    WIP

--- frontend/src/views/StudentActivity.vue
+++ frontend/src/views/StudentActivity.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="chartContainer" style="width: 100%; height: 400px;"></div>
+  <div ref="chartContainer"></div>
 </template>
 
 <script setup>
@@ -9,175 +9,117 @@ import * as d3 from 'd3'
 const chartContainer = ref(null)
 
 onMounted(() => {
-  let svg, x, y, xAxis, yAxis, bgBarsGroup, barsGroup
+   let svg, x, y, xAxis, yAxis, bgBarsGroup, barsGroup
 
-  // Helper: parse ISO month string to Date
-  const parseDate = d3.timeParse('%Y-%m')
+   // Helper: parse ISO month string to Date
+   const parseDate = d3.timeParse('%Y-%m')
 
-  const data1 = [
-    { date: parseDate('2023-01'), value: 30 },
-    { date: parseDate('2023-02'), value: 50 },
-    { date: parseDate('2023-03'), value: 45 },
-    { date: parseDate('2023-04'), value: 60 },
-    { date: parseDate('2023-05'), value: 40 },
-    { date: parseDate('2023-06'), value: 70 },
-    { date: parseDate('2023-07'), value: 55 },
-    { date: parseDate('2023-08'), value: 30 },
-    { date: parseDate('2023-09'), value: 50 },
-    { date: parseDate('2023-10'), value: 45 },
-    { date: parseDate('2023-11'), value: 60 },
-    { date: parseDate('2023-12'), value: 40 }
-  ]
+   const events = [
+      {
+         name: 'Event A',
+         start: new Date('2023-01-10'),
+         end: new Date('2023-01-20'),
+         value: 30
+      },
+      {
+         name: 'Event B',
+         start: new Date('2023-02-01'),
+         end: new Date('2023-02-18'),
+         value: 50
+      },
+      {
+         name: 'Event C',
+         start: new Date('2023-03-05'),
+         end: new Date('2023-03-15'),
+         value: 45
+      }
+   ]
 
-//   const data2 = data1.map(d => ({
-//     date: new Date(d.date),
-//     value: d.value * 0.7 + 10 // simulate alternate series
-//   }))
+   const margin = { top: 20, right: 20, bottom: 50, left: 40 }
 
-  const data2 = [
-    { date: parseDate('2023-01'), value: 30 },
-    { date: parseDate('2023-12'), value: 40 }
-  ]
+   function drawChart() {
+      const containerWidth = chartContainer.value.clientWidth
+      const width = containerWidth
+      const height = 400
 
-const events = [
-  {
-    name: 'Event A',
-    start: new Date('2023-01-10'),
-    end: new Date('2023-01-20'),
-    value: 30
-  },
-  {
-    name: 'Event B',
-    start: new Date('2023-02-01'),
-    end: new Date('2023-02-18'),
-    value: 50
-  },
-  {
-    name: 'Event C',
-    start: new Date('2023-03-05'),
-    end: new Date('2023-03-15'),
-    value: 45
-  }
-]
+      d3.select(chartContainer.value).selectAll('*').remove()
 
-  const margin = { top: 20, right: 20, bottom: 50, left: 40 }
+      svg = d3.select(chartContainer.value)
+         .append('svg')
+         .attr('viewBox', `0 0 ${width} ${height}`)
+         .attr('preserveAspectRatio', 'xMidYMid meet')
 
-  function drawChart() {
-    const containerWidth = chartContainer.value.clientWidth
-    const width = containerWidth
-    const height = 400
+      //  const allDates = data1.map(d => d.date)
+      const allStart = events.map(d => d.start)
+      const allEnd = events.map(d => d.end)
+      const dateMin = d3.min(allStart)
+      const dateMax = d3.max(allEnd)
 
-    d3.select(chartContainer.value).selectAll('*').remove()
+      x = d3.scaleTime()
+         .domain([d3.timeMonth.offset(dateMin, -0.5), d3.timeMonth.offset(dateMax, 0.5)])
+         .range([margin.left, width - margin.right])
 
-    svg = d3.select(chartContainer.value)
-      .append('svg')
-      .attr('viewBox', `0 0 ${width} ${height}`)
-      .attr('preserveAspectRatio', 'xMidYMid meet')
+      y = d3.scaleLinear()
+         .domain([0, d3.max([...events], d => d.value)]).nice()
+         .range([height - margin.bottom, margin.top])
 
-    const allDates = data1.map(d => d.date)
-    const dateMin = d3.min(allDates)
-    const dateMax = d3.max(allDates)
+      xAxis = svg.append('g')
+         .attr('transform', `translate(0,${height - margin.bottom})`)
+         .call(d3.axisBottom(x).ticks(d3.timeMonth.every(1)).tickFormat(d3.timeFormat('%b')))
 
-    x = d3.scaleTime()
-      .domain([d3.timeMonth.offset(dateMin, -0.5), d3.timeMonth.offset(dateMax, 0.5)])
-      .range([margin.left, width - margin.right])
+      yAxis = svg.append('g')
+         .attr('transform', `translate(${margin.left},0)`)
+         .call(d3.axisLeft(y))
 
-    y = d3.scaleLinear()
-      .domain([0, d3.max([...data1, ...data2], d => d.value)]).nice()
-      .range([height - margin.bottom, margin.top])
+      barsGroup = svg.append('g').attr('class', 'main-bars')
 
-    xAxis = svg.append('g')
-      .attr('transform', `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(x).ticks(d3.timeMonth.every(1)).tickFormat(d3.timeFormat('%b')))
+      drawBars(x)
 
-    yAxis = svg.append('g')
-      .attr('transform', `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y))
+      svg.call(
+         d3.zoom()
+         .scaleExtent([1, 5])
+         .translateExtent([[margin.left, 0], [width - margin.right, height]])
+         .on('zoom', zoomed)
+      )
+   }
 
-    bgBarsGroup = svg.append('g').attr('class', 'background-bars')
-    barsGroup = svg.append('g').attr('class', 'main-bars')
+   function drawBars(xScale) {
+      const eventRects = barsGroup.selectAll('rect')
+         .data(events, d => d.name)
 
-    drawBars(x)
+      eventRects.enter()
+         .append('rect')
+         .merge(eventRects)
+         .attr('x', d => xScale(d.start))
+         .attr('width', d => Math.max(1, xScale(d.end) - xScale(d.start)))
+         .attr('y', d => y(d.value)) // or use a fixed row layout like `margin.top + i * (barHeight + spacing)`
+         .attr('height', d => y(0) - y(d.value))
+         .attr('fill', 'steelblue')
+         // .on('mouseover', (event, d) => showTooltip(event, d))
+         // .on('mousemove', (event, d) => showTooltip(event, d))
+         // .on('mouseout', hideTooltip)
 
-    svg.call(
-      d3.zoom()
-        .scaleExtent([1, 5])
-        .translateExtent([[margin.left, 0], [width - margin.right, height]])
-        .on('zoom', zoomed)
-    )
-  }
+      eventRects.exit().remove()
+   }
 
-  function drawBars(xScale) {
-   //  const barWidth = (xScale(d3.timeMonth.offset(data1[1].date, 0)) - xScale(data1[0].date)) * 0.8
-   //  const fgWidth = barWidth * 0.4
-   //  const bgWidth = barWidth * 0.4
+   function zoomed(event) {
+      const transform = event.transform
+      const zx = transform.rescaleX(x)
 
-   //  // Background bars
-   //  const bgBars = bgBarsGroup.selectAll('rect')
-   //    .data(data2, d => d.date)
+      xAxis.call(d3.axisBottom(zx).ticks(d3.timeMonth.every(1)).tickFormat(d3.timeFormat('%b')))
+      drawBars(zx)
+   }
 
-   //  bgBars.enter()
-   //    .append('rect')
-   //    .merge(bgBars)
-   //    .attr('x', d => xScale(d.date) - bgWidth)
-   //    .attr('y', d => y(d.value))
-   //    .attr('width', bgWidth)
-   //    .attr('height', d => y(0) - y(d.value))
-   //    .attr('fill', '#ccc')
+   drawChart()
 
-   //  bgBars.exit().remove()
+   const resizeObserver = new ResizeObserver(() => {
+      drawChart()
+   })
 
-   //  // Foreground bars
-   //  const fgBars = barsGroup.selectAll('rect')
-   //    .data(data1, d => d.date)
+   resizeObserver.observe(chartContainer.value)
 
-   //  fgBars.enter()
-   //    .append('rect')
-   //    .merge(fgBars)
-   //    .attr('x', d => xScale(d.date))
-   //    .attr('y', d => y(d.value))
-   //    .attr('width', fgWidth)
-   //    .attr('height', d => y(0) - y(d.value))
-   //    .attr('fill', 'steelblue')
-
-   //  fgBars.exit().remove()
-
-   const eventRects = barsGroup.selectAll('rect')
-      .data(events, d => d.name)
-
-   eventRects.enter()
-      .append('rect')
-      .merge(eventRects)
-      .attr('x', d => xScale(d.start))
-      .attr('width', d => Math.max(1, xScale(d.end) - xScale(d.start)))
-      .attr('y', d => y(d.value)) // or use a fixed row layout like `margin.top + i * (barHeight + spacing)`
-      .attr('height', d => y(0) - y(d.value))
-      .attr('fill', 'steelblue')
-      // .on('mouseover', (event, d) => showTooltip(event, d))
-      // .on('mousemove', (event, d) => showTooltip(event, d))
-      // .on('mouseout', hideTooltip)
-
-   eventRects.exit().remove()
-}
-
-  function zoomed(event) {
-    const transform = event.transform
-    const zx = transform.rescaleX(x)
-
-    xAxis.call(d3.axisBottom(zx).ticks(d3.timeMonth.every(1)).tickFormat(d3.timeFormat('%b')))
-    drawBars(zx)
-  }
-
-  drawChart()
-
-  const resizeObserver = new ResizeObserver(() => {
-    drawChart()
-  })
-
-  resizeObserver.observe(chartContainer.value)
-
-  onBeforeUnmount(() => {
-    resizeObserver.disconnect()
-  })
+   onBeforeUnmount(() => {
+      resizeObserver.disconnect()
+   })
 })
 </script>
