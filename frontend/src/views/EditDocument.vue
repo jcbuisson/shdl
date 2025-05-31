9773ef2d05501d14f3b9596b17965f290ca0711e
JC BUISSON <buisson@enseeiht.fr>
2025-05-31 14:41:48 +0200

    WIP

--- frontend/src/views/EditDocument.vue
+++ frontend/src/views/EditDocument.vue
@@ -2,17 +2,22 @@
    <codemirror
       v-if="selectedDoc"
       v-model="selectedDoc.content"
+      :selection="selectedDoc.selection"
       :extensions="selectedDoc.extensions"
       placeholder="Start coding here..."
       class="fill-height"
       @change="onChangeDebounced($event)"
+      @ready="handleEditorReady"
    />
 </template>
 
 <script setup>
-import { ref, computed, watch, onUnmounted } from 'vue'
+import { ref, shallowRef, watch, onUnmounted } from 'vue'
 import { Codemirror } from 'vue-codemirror';
 import { useDebounceFn } from '@vueuse/core'
+import { javascript } from '@codemirror/lang-javascript'
+import { basicSetup } from 'codemirror'
+import { history } from '@codemirror/commands'
 // import { myLangSupport } from "/src/lib/shdl"
 
 import { addPerimeter as addUserDocumentPerimeter, update as updateUserDocument } from '/src/use/useUserDocument'
@@ -25,6 +30,12 @@ const props = defineProps({
       type: String,
    },
 })
+
+const currentEditorView = shallowRef()
+const handleEditorReady = (payload) => {
+   console.log('ready', payload)
+   currentEditorView.value = payload.view
+}
 
 let perimeter
 
@@ -45,6 +56,8 @@ watch(() => props.document_uid, async (uid, previous_uid) => {
          const newDoc = {
             content: document.text,
             selection: null,
+            // extensions: [javascript()/*, oneDark*/, basicSetup, history()/*, myLangSupport()*/]
+            extensions: [javascript()],
          }
          uid2docDict[uid] = newDoc
          selectedDoc.value = newDoc
