73499438b686a82aa6f4ac0d6c0e4956a6d68d37
JC BUISSON <buisson@enseeiht.fr>
2025-06-24 18:45:27 +0200

    WIP

--- frontend/src/views/users/ManageUsers.vue
+++ frontend/src/views/users/ManageUsers.vue
@@ -4,16 +4,30 @@
       <template v-slot:left-panel>
          <!-- makes the layout a vertical stack filling the full height -->
          <v-card class="d-flex flex-column fill-height">
+
             <!-- Toolbar (does not grow) -->
-            <v-toolbar color="red-darken-4" density="compact">
-               <v-btn readonly icon="mdi-magnify" variant="text"></v-btn>
-               <v-text-field v-model="filter" single-line></v-text-field>
+            <v-toolbar color="red-darken-4" ddensity="compact">
+               <v-text-field v-model="nameFilter" label="Recherche par nom..." class="px-2" single-line></v-text-field>
                <v-btn icon="mdi-plus" variant="text" @click="addUser"></v-btn>
             </v-toolbar>
+
+            <!-- Filter by group (does not grow) -->
+            <div class="px-2">
+               <v-select
+                  variant="underlined"
+                  v-model="userGroups"
+                  @update:modelValue="onGroupChange"
+                  :items="groupList"
+                  item-title="name"
+                  item-value="uid"
+                  label="Recherche par groupe..."
+               ></v-select>
+            </div>
+
          
             <!-- Fills remaining vertical space -->
             <div class="d-flex flex-column flex-grow-1 overflow-auto">
-               <v-list-item three-line v-for="(userAndGroups, index) in userAndGroupsList" :key="index" :value="userAndGroups?.user" @click="selectUser(userAndGroups.user)" :active="selectedUser?.uid === userAndGroups?.user.uid">
+               <v-list-item three-line v-for="(userAndGroups, index) in filteredUserAndGroupList" :key="index" :value="userAndGroups?.user" @click="selectUser(userAndGroups.user)" :active="selectedUser?.uid === userAndGroups?.user.uid">
                   <template v-slot:prepend>
                      <v-avatar @click="onAvatarClick(userAndGroups.user)">
                         <v-img :src="userAndGroups?.user.pict"></v-img>
@@ -50,7 +64,7 @@
 
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
 import { useRoute} from 'vue-router'
 import { Observable, from, map, of, merge, combineLatest, forkJoin, firstValueFrom } from 'rxjs'
 import { mergeMap, switchMap, concatMap, scan, tap, catchError, take, debounceTime } from 'rxjs/operators'
@@ -81,10 +95,13 @@ const props = defineProps({
    },
 })
 
-const filter = ref('')
+const groupList = useObservable(groups$({}))
+const userGroups = ref([])
 
-// ?? marche mal si on remplace switchMap par mergeMap
-const userAndGroupsList = useObservable(users$({}).pipe(
+const nameFilter = ref('')
+const groupFilter = ref('')
+
+const userAndGroups$ = users$({}).pipe(
    switchMap(users => 
       guardCombineLatest(
          users.map(user =>
@@ -97,7 +114,28 @@ const userAndGroupsList = useObservable(users$({}).pipe(
          )
       )
    ),
-))
+)
+const userAndGroupsList = useObservable(userAndGroups$)
+
+function onGroupChange(uid) {
+   groupFilter.value = uid
+}
+
+const filteredUserAndGroupList = computed(() => {
+   if (!userAndGroupsList.value) return []
+   const nameFilter_ = nameFilter.value.toLowerCase()
+   return userAndGroupsList.value.filter(ug => {
+      if (nameFilter_.length === 0) return true
+      if (ug.user.firstname.toLowerCase().indexOf(nameFilter_) > -1) return true
+      if (ug.user.lastname.toLowerCase().indexOf(nameFilter_) > -1) return true
+      return false
+   }).filter(ug => {
+      if (!groupFilter.value) return true
+      if (ug.groups.map(grp => grp.uid).includes(groupFilter.value)) return true
+      return false
+   })
+})
+
 
 async function addUser() {
    router.push(`/home/${props.signedinUid}/users/create`)
