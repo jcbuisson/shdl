1e8b8f879cad2a926cf7605c015eafcb3d77649c
JC BUISSON <buisson@enseeiht.fr>
2025-10-11 08:06:01 +0200

    WIP

--- frontend/src/components/EditSHDLDocument.vue
+++ frontend/src/components/EditSHDLDocument.vue
@@ -18,14 +18,15 @@
 </template>
 
 <script setup>
-import { ref, watch, onUnmounted, onBeforeUnmount } from 'vue'
+import { ref, watch, onMounted, onUnmounted, onBeforeUnmount } from 'vue'
 import { EditorView } from 'codemirror'
 import { useDebounceFn } from '@vueuse/core'
 import { map } from 'rxjs'
+import { useObservable } from "@vueuse/rxjs"
 
 import { keymap, lineNumbers } from '@codemirror/view'
 import { EditorState } from '@codemirror/state'
-import { defaultKeymap, indentWithTab, history, historyKeymap } from '@codemirror/commands'
+import { defaultKeymap, indentWithTab } from '@codemirror/commands'
 
 import router from '/src/router'
 
@@ -51,6 +52,7 @@ const props = defineProps({
 const editorContainer = ref(null)
 let view = null
 
+
 onBeforeUnmount(() => {
    if (view) {
       view.destroy()
@@ -66,11 +68,44 @@ const currentDocument = ref()
 
 let updateUid
 
+// const documentsOfUser = useObservable(userDocuments$({ user_uid: props.user_uid }))
+
 function userDocument$(uid) {
    return userDocuments$({ uid }).pipe(
       map(documents => documents.length > 0 ? documents[0] : null)
    )
 }
+
+onMounted(() => {
+   const editable = props.signedinUid === props.user_uid
+   const customTheme = EditorView.theme({
+      "&": {
+         fontSize: "13px",
+      }
+   })
+   const state = EditorState.create({
+      // doc: "Hello",
+      extensions: [
+         keymap.of([
+            indentWithTab, // makes Tab insert indentation
+            ...defaultKeymap
+         ]),
+         lineNumbers(),
+         myLang,
+         customTheme,
+         EditorView.editable.of(editable),
+         EditorView.updateListener.of((update) => {
+            if (update.changes) {
+               onTextChangeDebounced(update.state.doc.toString())
+            }
+         })
+      ]
+   })
+   view = new EditorView({
+      state,
+      parent: editorContainer.value
+   })
+})
 
 watch(() => props.document_uid, async (uid, previous_uid) => {
    if (view) {
@@ -85,17 +120,13 @@ watch(() => props.document_uid, async (uid, previous_uid) => {
    const state = EditorState.create({
       // doc: "Hello",
       extensions: [
-         lineNumbers(),
-         indentUnit.of("   "),
-         history(), // adds undo/redo support
          keymap.of([
-            indentWithTab,
-            ...defaultKeymap,
-            ...historyKeymap // enables Cmd+Z / Cmd+Shift+Z
+            indentWithTab, // makes Tab insert indentation
+            ...defaultKeymap
          ]),
+         lineNumbers(),
          myLang,
          customTheme,
-
          EditorView.editable.of(editable),
          EditorView.updateListener.of((update) => {
             if (update.changes) {
@@ -115,7 +146,7 @@ watch(() => props.document_uid, async (uid, previous_uid) => {
       currentDocument.value = doc
       console.log('xxx', doc)
 
-      setEditorDoc(doc.text)
+      forceUpdateCode(doc.text)
 
       if (doc.type === 'shdl') {
          analyzeSHDLDocument(doc)
@@ -129,23 +160,19 @@ onUnmounted(() => {
    subscription2 && subscription2.unsubscribe()
 })
 
-// safe programmatic update that preserves history (thank you ChatGPT)
-function setEditorDoc(newText) {
-   if (!view) return;
-   const current = view.state.doc.toString();
-   if (current === newText) return;
-
+function forceUpdateCode(newValue) {
+   const pos = view.state.selection.main.head
    view.dispatch({
-      changes: { from: 0, to: view.state.doc.length, insert: newText },
-      userEvent: "input"
-   });
+      changes: { from: 0, to: view.state.doc.length, insert: newValue },
+      selection: { anchor: pos },
+   })
 }
 
 async function onTextChange(text) {
    if (currentDocument.value.text === text) return
 
    // change editor content
-   setEditorDoc(text)
+   forceUpdateCode(text)
 
    if (currentDocument.value.type === 'shdl') {
       analyzeSHDLDocument(currentDocument.value)
