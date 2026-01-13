a8fc9f0565ce77d4d99df871157d0a89f414680c
JC BUISSON <buisson@enseeiht.fr>
2026-01-13 07:40:14 +0100

    WIP

--- backend/scripts/excel-evaluation.js
+++ backend/scripts/excel-evaluation.js
@@ -54,7 +54,7 @@ async function createExcel() {
          const user = await prisma.user.findUnique({ where: { uid: userGroupRelation.user_uid }});
          users.push(user);
       }
-      const sortedUsers = users.sort((a, b) => a.name.localeCompare(b.name));
+      const sortedUsers = users.sort((a, b) => a.firstname.localeCompare(b.firstname));
       for (const user of sortedUsers) {
          const row = {
             lastname: user.lastname,
